352a1980f8bafcfcc6a0411ac9ffc0b4417b68f8
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-08-23 07:42:36 -0700

    Time: 3 ms (5.26%), Space: 41.9 MB (15.08%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Arrays.stream(candies).max().getAsInt();
+        ArrayList result = new ArrayList<>();
+        
+        for (int i = 0; i < candies.length; i++) {
+            result.add((candies[i] + extraCandies) >= max);
+        }
+        
+        return result;
+    }
+}
