1be3a566770246365c3886bdba48b21b0054b5b4
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-09-08 04:44:23 -0700

    Time: 0 ms (100.00%), Space: 40.9 MB (69.23%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+    }
+
+    public int partition(int[] nums, int low, int high) {
+        int pivot = nums[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (nums[i] <= pivot && i <= high - 1) {
+                i++;
+            }
+            while (nums[j] > pivot && j >= low + 1) {
+                j--;
+            }
+            if (i < j) {
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, low, j);
+        return j;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(nums, low, high);
+            quickSort(nums, low, partitionIndex - 1);
+            quickSort(nums, partitionIndex + 1, high);
+        }
+    }
+}
