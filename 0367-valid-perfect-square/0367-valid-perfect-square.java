be299b2b5336d0e167e1267efb118544d138327a
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-10-02 22:15:49 -0700

    Time: 0 ms (100.00%), Space: 38.4 MB (99.52%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,25 @@
+class Solution {
+   public boolean isPerfectSquare(int num) {
+        if (num == 0 || num == 1) {
+            return true;
+        }
+    
+        long low = 1; 
+        long high = num;
+    
+        while (low <= high) { 
+            long mid = low + (high - low) / 2;
+            long square = mid * mid;
+            
+            if (square == num) {
+                return true;
+            } else if (square > num) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+}
