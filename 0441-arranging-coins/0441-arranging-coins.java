8e1997afdac22c604138241947b9a0326b4eb855
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-10-02 23:19:01 -0700

    Time: 1 ms (100.00%), Space: 39.9 MB (30.85%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,11 +1,21 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int row = 1;
-        int count = 0;
-        while (n > 0) {
-            ++row;
-            n -= row;
-            count++;
-        }return count; 
+        int left = 1;
+        int right = n;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long gaussCheck = (long)mid * (mid + 1) / 2; 
+            if (gaussCheck > n) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        if ((long)left * (left + 1) / 2 > n) {
+            return left - 1;
+        }
+        
+        return left;
     }
 }
