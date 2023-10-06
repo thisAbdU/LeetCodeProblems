ab74dca026fef9c9ea1913222ee74b318a22d967
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-10-06 00:51:23 -0700

    Time: 0 ms (100.00%), Space: 44 MB (26.17%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,12 +1,21 @@
 class Solution {
     public int countNegatives(int[][] grid) {
         int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[i].length; j++){
-                if(grid[i][j] < 0){
-                    count++;
-                }
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int row = 0;
+        int col = cols - 1;
+
+        while (row < rows && col >= 0) {
+            if (grid[row][col] < 0) {
+                count += (rows - row);
+                col--;
+            } else {
+                row++;
             }
-        }return count;
+        }
+
+        return count;
     }
 }
