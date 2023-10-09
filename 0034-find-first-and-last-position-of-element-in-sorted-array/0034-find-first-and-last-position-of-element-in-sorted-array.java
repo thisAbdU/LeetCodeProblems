3220495f3eea3498e1b3e2fb4883181bccaf8ef5
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-10-09 12:56:39 -0700

    Time: 0 ms (100.00%), Space: 44.4 MB (38.64%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
        int left = 0;
-        int right = nums.length - 1;
-        int[] result = new int[2];
-        
-        // Initialize result to -1, indicating no target found initially
+       int right = nums.length - 1;
+
+       int[] result = new int[2];
         result[0] = -1;
         result[1] = -1;
 
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
+       while (left <= right) {
+        int mid = left + (right - left)/2;
 
-            if (nums[middle] == target) {
-                // Found the target, update the result
-                result[0] = middle;
-                result[1] = middle;
+        if (nums[mid] == target) {
+            result[0] = mid;
+            result[1] = mid;
 
-                // Expand the search to the left and right for the first and last positions
-                int leftPointer = middle - 1;
-                int rightPointer = middle + 1;
+            int leftPointer = mid - 1;
+            int rightPointer = mid + 1;
 
-                while (leftPointer >= 0 && nums[leftPointer] == target) {
-                    result[0] = leftPointer;
-                    leftPointer--;
-                }
-
-                while (rightPointer < nums.length && nums[rightPointer] == target) {
-                    result[1] = rightPointer;
-                    rightPointer++;
-                }
-
-                return result;
-            } else if (nums[middle] < target) {
-                left = middle + 1;
-            } else {
-                right = middle - 1;
+            while (leftPointer >= 0 && nums[leftPointer] == target) {
+                result[0] = leftPointer;
+                leftPointer--;
             }
+            while (rightPointer < nums.length && nums[rightPointer] == target) {
+                result[1] = rightPointer;
+                rightPointer++;
+            }
+
+            return result;
+
+        } else if(nums[mid] < target) {
+            left = mid + 1;
+
+        }else{
+            right = mid - 1;
+
         }
 
-        return result; 
+       }
+       return result;
     }
 }
