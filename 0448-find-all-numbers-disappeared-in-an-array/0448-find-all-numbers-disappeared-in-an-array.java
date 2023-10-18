aa2f9174be45f5ca25451d00c91a2f8c4b5920bf
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-10-18 00:41:46 -0700

    Time: 7 ms (37.28%), Space: 53.3 MB (54.77%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,20 +1,36 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-     List<Integer> disappearedNumbers = new ArrayList<>();
-        
+        List<Integer> disappearedNumbers = new ArrayList<>();
+        sort(nums);
+
         for (int i = 0; i < nums.length; i++) {
-            int val = Math.abs(nums[i]) - 1;
-            if (nums[val] > 0) {
-                nums[val] = -nums[val];
-            }
-        }
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
+            if (nums[i] != i + 1) {
                 disappearedNumbers.add(i + 1);
             }
         }
-        
         return disappearedNumbers;
+    }
+    
+    static int[] sort(int[] nums) {
+       int i = 0;
+       while (i < nums.length) {
+        int correct = nums[i] - 1;
+        
+        if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }
+            else{
+                  i++;
+            }
+
+       }
+       return nums;
+        
+    }
+
+    static void swap(int[] nums, int numOne, int numTwo){
+        int temp = nums[numOne];
+        nums[numOne] = nums[numTwo];
+        nums[numTwo] = temp;
     }
 }
