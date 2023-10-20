55d486ff958b490464f900d45acff632809b7fe1
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-10-20 00:59:00 -0700

    Time: 1 ms (99.90%), Space: 48.2 MB (59.64%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+      int count = 0;
+      int candidate = nums[0];
+
+        for (int i : nums) {
+            if (count == 0) {
+                candidate = i;
+            }
+
+            if (candidate == i) {
+                count++;
+            }else{
+                count--;
+            }
+        } return candidate;  
+    }
+}
