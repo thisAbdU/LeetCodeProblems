55e4d5deae629062a5c6f58893c4fe2d05c48a70
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-10-25 04:53:20 -0700

    Time: 4 ms (58.83%), Space: 43.4 MB (50.26%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,36 +1,22 @@
 class Solution {
-   public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        // Build the mapping of next greater elements for nums2
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
         int[] output = new int[nums1.length];
-       
+        
         for (int i = 0; i < nums1.length; i++) {
-            int j = 0; // Reset the index for nums2
-            while (j < nums2.length) {
-                if (nums1[i] != nums2[j]) {
-                    j++;
-                } else {
-                    output[i] = findMax(nums1[i], nums2, j);
-                    break; // Exit the loop once a match is found
-                }
-            }
+            output[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
         }
-
+        
         return output;
-    }
-    
-    static int findMax(int i, int[] nums2, int start) {
-        int max = i;
-        
-        for (int k = start; k < nums2.length; k++) {
-            if (max < nums2[k]) {
-                max = nums2[k];
-                break; // Exit the loop once a greater element is found
-            }
-        }
-        
-        if (max == i) {
-            return -1;
-        } else {
-            return max;
-        }
     }
 }
