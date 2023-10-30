b777163663243aaab506d3d115093e750461fe19
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-10-30 00:02:41 -0700

    Time: 26 ms (86.16%), Space: 59.7 MB (55.35%) - LeetHub

---
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int playerIdx = 0;
+        int trainerIdx = 0;
+        int maxMatches = 0;
+
+        while (playerIdx < players.length && trainerIdx < trainers.length) {
+            if (players[playerIdx] <= trainers[trainerIdx]) {
+                maxMatches++;
+                playerIdx++;
+            }
+            trainerIdx++;
+        }
+
+        return maxMatches;
+    }
+}
+
