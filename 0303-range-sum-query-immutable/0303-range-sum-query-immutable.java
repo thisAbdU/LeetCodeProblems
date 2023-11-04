cd8334d1825be1416648719c3cfaf143d216c7fc
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-11-04 07:05:24 -0700

    Time: 8 ms (77.69%), Space: 48.7 MB (38.10%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,21 +1,30 @@
 class NumArray {
-    private int[] nums; 
-    
-    public NumArray(int[] nums) {
-        this.nums = nums; 
-    }
-    
-    public int sumRange(int left, int right) {
-        int sum = 0;
-        for (int i = left; i <= right; i++) {
-            sum += nums[i];
-        }
-        return sum;
-    }
-}
+    private int[] sumsArray;
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(left, right);
- */
+    public NumArray(int[] nums) {
+        if (nums.length > 0) {
+            sumsArray = new int[nums.length];
+            sumsArray[0] = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                sumsArray[i] = sumsArray[i - 1] + nums[i];
+            }
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if (sumsArray == null) {
+            return 0; 
+        }
+        if (left == 0) {
+            return sumsArray[right];
+        } else {
+            return sumsArray[right] - sumsArray[left - 1];
+        }
+    }
+
+    /**
+     * Your NumArray object will be instantiated and called as such:
+     * NumArray obj = new NumArray(nums);
+     * int param_1 = obj.sumRange(left, right);
+     */
+}
