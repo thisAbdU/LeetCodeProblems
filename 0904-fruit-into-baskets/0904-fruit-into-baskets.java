f9e870bc36876141e6532860396f47e4b79b7646
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-11-08 03:42:14 -0800

    Time: 48 ms (38.90%), Space: 55.2 MB (14.36%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,41 +1,25 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class Solution {
+class Solution {
     public int totalFruit(int[] fruits) {
-        if (fruits == null || fruits.length == 0) {
-            return 0;
-        }
-        
-        Map<Integer, Integer> fruitType = new HashMap<>();
-        int maxSum = 0;
-        int count = 0;
-        int left = 0;
-        
-        for (int right = 0; right < fruits.length; right++) {
-            int currentFruit = fruits[right];
-            
-            if (fruitType.containsKey(currentFruit)) {
-                fruitType.put(currentFruit, fruitType.get(currentFruit) + 1);
-            } else {
-                fruitType.put(currentFruit, 1);
-                count++;
-            }
-            
-            while (count > 2) {
-                int leftFruit = fruits[left];
-                if (fruitType.get(leftFruit) == 1) {
-                    fruitType.remove(leftFruit);
-                    count--;
-                } else {
-                    fruitType.put(leftFruit, fruitType.get(leftFruit) - 1);
+        int maxFruit = 0;
+        int i = 0;
+        int j = 0;
+        Map<Integer, Integer> fruitCount = new HashMap<>();
+
+        while (j < fruits.length) {
+            fruitCount.put(fruits[j], fruitCount.getOrDefault(fruits[j], 0) + 1);
+
+            while (fruitCount.size() > 2) {
+                fruitCount.put(fruits[i], fruitCount.get(fruits[i]) - 1);
+                if (fruitCount.get(fruits[i]) == 0) {
+                    fruitCount.remove(fruits[i]);
                 }
-                left++;
+                i++;
             }
-            
-            maxSum = Math.max(maxSum, right - left + 1);
+
+            maxFruit = Math.max(maxFruit, j - i + 1);
+            j++;
         }
-        
-        return maxSum;
+
+        return maxFruit;
     }
 }
