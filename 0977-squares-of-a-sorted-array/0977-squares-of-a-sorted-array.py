82d5dc5ea0024f6dcfa5c81babb8c2156c4e52bf
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-11-12 07:02:37 -0800

    Time: 206 ms (36.23%), Space: 18.6 MB (23.88%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,9 +1,19 @@
-class Solution {
-    public int[] sortedSquares(int[] nums) {
-        for(int i=0; i < nums.length; i++){
-            nums[i] = nums[i] * nums[i];
-        }
-        Arrays.sort(nums);
-        return nums;
-    }
-}
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        newArr = [0] * n  # Initialize newArr with appropriate length
+        
+        i = 0
+        j = n - 1
+        k = n - 1
+        
+        while i <= j:
+            if abs(nums[i]) > abs(nums[j]):
+                newArr[k] = nums[i] ** 2
+                i += 1
+            else:
+                newArr[k] = nums[j] ** 2
+                j -= 1
+            k -= 1
+        
+        return newArr
