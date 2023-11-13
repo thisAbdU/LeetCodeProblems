86dc5172cf838a38c3cdd3b17070cf3f8d3bbdb4
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-11-13 03:59:08 -0800

    Time: 43 ms (87.43%), Space: 17.8 MB (20.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,13 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-      String cleanedString = convertToLowerCaseRemoveNonAlphaNumeric(s);
-
-        int start = 0;
-        int end = cleanedString.length() -1;
-
-        while (start < end) {
-            if (cleanedString.charAt(start) != cleanedString.charAt(end)) {
-                return false;
-            }
-            
-            start++;
-            end--;
-
-        }return true;
-    }
-
-    public String convertToLowerCaseRemoveNonAlphaNumeric(String string){
-        String word = string.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        return word;
-    }
-}
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned_string = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        i = 0
+        j = len(cleaned_string) - 1
+        
+        while i < j:
+            if cleaned_string[i] != cleaned_string[j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
