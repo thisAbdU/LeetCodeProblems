3b4061c048649c6e5f66e496c59a3a84f03b4945
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-11-13 03:29:10 -0800

    Time: 3 ms (90.09%), Space: 43.9 MB (15.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        // Compare the sorted arrays for equality
+        return Arrays.equals(sChars, tChars);
+    }
+}
