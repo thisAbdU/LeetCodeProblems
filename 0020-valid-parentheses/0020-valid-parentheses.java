b101bed9680abce6955def07c3f869d396fc613f
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-11-13 04:47:07 -0800

    Time: 2 ms (83.37%), Space: 40.9 MB (6.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,30 +1,22 @@
 import java.util.Stack;
 
-class Solution {
+public class Solution {
     public boolean isValid(String s) {
-      Stack<Character> chars = new Stack<>();
-        
-      for(Character ch: s.toCharArray()){
-          if(ch == '[' || ch.equals('{') || ch.equals('(')){
-              chars.push(ch);
-          }
-          else{
-              if(ch.equals('}')){
-                   if(chars.isEmpty() || chars.pop() != '{'){
-                       return false;
-                   }
-              }if(ch.equals(')')){
-                   if(chars.isEmpty() || chars.pop() != '('){
-                       return false;
-                   }
-              }if(ch.equals(']')){
-                   if(chars.isEmpty() || chars.pop() != '['){
-                       return false;
-                   }
-              }
-    
-      }
-    } return chars.isEmpty();
-}
-    
+        Stack<Character> brackets = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '[' || ch == '{' || ch == '(') {
+                brackets.push(ch);
+            } else {
+                if (brackets.isEmpty()) {
+                    return false; // No opening bracket for the closing one
+                }
+                char top = brackets.pop();
+                if ((ch == ']' && top != '[') || (ch == '}' && top != '{') || (ch == ')' && top != '(')) {
+                    return false; // Mismatched closing and opening brackets
+                }
+            }
+        }
+        return brackets.isEmpty(); // Ensure all opening brackets are closed
+    }
 }
