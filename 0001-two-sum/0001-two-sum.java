cb665a722c41bb7dbdad35717f260586f4eb6bec
Abdulfetah Yi. Abduljelil <abdulfetah.yishak@aait.edu.et>
2023-11-18 21:52:29 -0800

    Time: 2 ms (82.09%), Space: 43.7 MB (33.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-for(int i = 0; i < nums.length; i++){
-    if(map.containsKey(target - nums[i])) {
-        return new int[] {map.get(target - nums[i]), i};
-    }map.put(nums[i], i);
-}
-return null;
+        Map<Integer, Integer> numsPos = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(numsPos.containsKey(target - nums[i])){
+                return new int[]{numsPos.get(target - nums[i]), i};
+            }
+            else{
+                numsPos.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
     }
 }
+        
