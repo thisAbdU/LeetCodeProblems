7c6c61dae72a7c91595591b2d829a7305beae29e
Abdulfetah Yi. Abduljelil <abdulfetaheishak@gmail.com>
2024-08-27 08:26:25 +0300

    Time: 1247 ms (86.96%), Space: 37.3 MB (54.12%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,42 +1,16 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        counts = [0] * n
-        indices = list(range(n))
+        sl = SortedList()
+        nums.reverse()
         
-        def mergesort(left, right):
-            if right - left <= 1:
-                return
-            
-            mid = (left + right) // 2
-            mergesort(left, mid)
-            mergesort(mid, right)
-            merge(left, mid, right)
+        res = []
         
-        def merge(left, mid, right):
-            i = left
-            j = mid
-            temp = []
-            while i < mid and j < right:
-                if nums[indices[i]] <= nums[indices[j]]:
-                    temp.append(indices[i])
-                    counts[indices[i]] += (j - mid) 
-                    i += 1
-                else:
-                    temp.append(indices[j])
-                    j += 1
- 
-            while i < mid:
-                temp.append(indices[i])
-                counts[indices[i]] += (j - mid)
-                i += 1
+        for n in nums:
+            indx = sl.bisect_left(n)
+            res.append(indx)
+            sl.add(n)
             
-            while j < right:
-                temp.append(indices[j])
-                j += 1
-            
-            for k in range(left, right):
-                indices[k] = temp[k - left]
- 
-        mergesort(0, n)
-        return counts
+        res.reverse()
+        return res
