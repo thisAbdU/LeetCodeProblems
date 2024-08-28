c68dced5f0aa61648f503430f74b980ca9286db6
Abdulfetah Yi. Abduljelil <abdulfetaheishak@gmail.com>
2024-08-28 14:28:07 +0300

    Time: 520 ms (93.36%), Space: 28.6 MB (55.11%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,23 +1,26 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        adj = defaultdict(list)
-
-        for node in range(len(edges)):
-            adj[edges[node][0]].append((edges[node][1],succProb[node]))
-            adj[edges[node][1]].append((edges[node][0],succProb[node]))
+        graph = [[] for _ in range(n)]
+        for i, (u, v) in enumerate(edges):
+            graph[u].append((v, succProb[i]))
+            graph[v].append((u, succProb[i]))
         
-        def dijkstra(src,dst):
-            heap = [(-1,src)]
-            visited = set()
-            while heap:
-                cost,node = heapq.heappop(heap)
-                if node == dst:
-                    return - cost
-                if node in visited:
-                    continue
-                visited.add(node)
-                for nei,cost_2 in adj[node]:
-                    if nei not in visited:
-                        heapq.heappush(heap,((cost * cost_2),nei))
-            return 0
-        return dijkstra(start_node,end_node)
+        max_heap = [(-1.0, start_node)] 
+        max_prob = [0.0] * n  
+        max_prob[start_node] = 1.0 
+        
+        while max_heap:
+            curr_prob, node = heappop(max_heap)
+            curr_prob = -curr_prob
+            
+            if node == end_node:
+                return curr_prob
+            
+
+            for neighbor, prob in graph[node]:
+                new_prob = curr_prob * prob
+                if new_prob > max_prob[neighbor]:
+                    max_prob[neighbor] = new_prob
+                    heappush(max_heap, (-new_prob, neighbor))
+        
+        return 0.0
